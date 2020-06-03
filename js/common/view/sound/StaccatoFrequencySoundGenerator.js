b218ff34bca3d627de5cb525f3ad84f5f1e5618c
zepumph <michael.kauzmann@colorado.edu>
2020-06-02 17:30:29 -0800

    add better staccato in proportion sounds (success sounds); fade out when leaving proportion, https://github.com/phetsims/ratio-and-proportion/issues/9

--- js/common/view/sound/StaccatoFrequencySoundGenerator.js
+++ js/common/view/sound/StaccatoFrequencySoundGenerator.js
@@ -1,7 +1,7 @@
 // Copyright 2020, University of Colorado Boulder
 
 /**
- *
+ * TODO: isInteractingProperty should reset this.playedSuccessYet = false;???
  * Marimba bonks that change frequency based on the fitness of the Proportion
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
@@ -11,10 +11,13 @@ import merge from '../../../../../phet-core/js/merge.js';
 import SoundClip from '../../../../../tambo/js/sound-generators/SoundClip.js';
 import SoundGenerator from '../../../../../tambo/js/sound-generators/SoundGenerator.js';
 import brightMarimbaSound from '../../../../../tambo/sounds/bright-marimba_mp3.js';
+import dingRingOutSound from '../../../../sounds/c4-ding-ring-out_mp3.js';
+import glockMarimbaCMaj7ArpeggioSound from '../../../../sounds/glock-marimba-c-maj-7-arp_mp3.js';
 import ratioAndProportion from '../../../ratioAndProportion.js';
 import designingProperties from '../../designingProperties.js';
 import RatioAndProportionQueryParameters from '../../RatioAndProportionQueryParameters.js';
-import SoundClipChord from './SoundClipChord.js';
+
+const SUCCESS_OUTPUT_LEVEL = 1;
 
 class StaccatoFrequencySoundGenerator extends SoundGenerator {
 
@@ -29,17 +32,25 @@ class StaccatoFrequencySoundGenerator extends SoundGenerator {
     }, options );
     super( options );
 
-    // TODO: wire these up with better sounds
-    this.singleSuccessSoundClip = new SoundClip( brightMarimbaSound, {
-      initialPlaybackRate: 2
-    } );
-    this.singleArpeggiatedChord = new SoundClipChord( brightMarimbaSound, {
-      chordPlaybackRates: [ 1, Math.pow( 2, 4 / 12 ), Math.pow( 2, 7 / 12 ), Math.pow( 2, 11 / 12 ) ],
-      arpeggiate: true
-    } );
-    this.singleSuccessSoundClip.connect( this.masterGainNode );
-    this.singleArpeggiatedChord.connect( this.masterGainNode );
+    const singleSuccessSoundClip = new SoundClip( dingRingOutSound );
+    const singleArpeggiatedChord = new SoundClip( glockMarimbaCMaj7ArpeggioSound );
+    singleSuccessSoundClip.connect( this.masterGainNode );
+    singleArpeggiatedChord.connect( this.masterGainNode );
 
+    // @private
+    this.successSoundClip = null;
+    designingProperties.staccatoSuccessSoundSelectorProperty.link( selector => {
+      if ( selector === 0 ) {
+        this.successSoundClip = singleSuccessSoundClip;
+      }
+      else if ( selector === 1 ) {
+        this.successSoundClip = singleArpeggiatedChord;
+      }
+      else {
+        assert && assert( false, 'unexpected staccato success sound selected' );
+      }
+      this.successSoundClip.setOutputLevel( SUCCESS_OUTPUT_LEVEL );
+    } );
 
     this.tonicSoundClip = new SoundClip( brightMarimbaSound );
     this.tonicSoundClip.connect( this.masterGainNode );
@@ -50,6 +61,25 @@ class StaccatoFrequencySoundGenerator extends SoundGenerator {
     this.timeLinearFunction = new LinearFunction( fitnessRange.min, fitnessRange.max, 500,
       RatioAndProportionQueryParameters.staccatoMinRepeatTime, true );
     this.timeSinceLastPlay = 0;
+    this.oldFitness = this.fitnessProperty.value;
+  }
+
+  /**
+   * @param {number} fitness
+   * @returns {number}
+   */
+  getNormalizedFitness( fitness ) {
+    return ( fitness - this.fitnessRange.min ) / this.fitnessRange.getLength();
+  }
+
+  /**
+   * TODO: why .05?
+   * Is the ratio currently considered a success as it pertains to this sound generator
+   * @param {number} fitness
+   * @returns {boolean}
+   */
+  isInSuccessfulRatio( fitness ) {
+    return 1 - this.getNormalizedFitness( fitness ) < .05;
   }
 
   /**
@@ -62,32 +92,28 @@ class StaccatoFrequencySoundGenerator extends SoundGenerator {
 
     this.remainingFadeTime = Math.max( this.remainingFadeTime - dt, 0 );
 
-    const normalizedFitness = ( this.fitnessProperty.value - this.fitnessRange.min ) / this.fitnessRange.getLength();
+    const newFitness = this.fitnessProperty.value;
+    const normalizedFitness = this.getNormalizedFitness( newFitness );
+    const isInRatio = this.isInSuccessfulRatio( newFitness );
+    if ( isInRatio && !this.playedSuccessYet ) {
 
-    if ( this.timeSinceLastPlay > this.timeLinearFunction( normalizedFitness ) ) {
-
-      // success condition
-      if ( 1 - normalizedFitness < .05 ) {
-
-        if ( !this.ratioSuccess ) {
-          if ( designingProperties.staccatoSuccessSoundSelectorProperty.value === 0 ) {
-            // TODO: make ding sound
-            this.singleSuccessSoundClip.play();
-          }
-          else if ( designingProperties.staccatoSuccessSoundSelectorProperty.value === 1 ) {
-
-            // TODO: make the better argeppi chord sound clip
-            this.singleArpeggiatedChord.play();
-          }
-          this.ratioSuccess = true;
-        }
-      }
-      else {
-        this.ratioSuccess = false;
-        this.tonicSoundClip.play();
-        this.timeSinceLastPlay = 0;
-      }
+      // TODO: is it possible that this will just bring a previous playing's reverb back to life and the play another instance on top of it?
+      this.successSoundClip.setOutputLevel( SUCCESS_OUTPUT_LEVEL );
+      this.successSoundClip.play();
+      this.playedSuccessYet = true;
     }
+    else if ( this.timeSinceLastPlay > this.timeLinearFunction( normalizedFitness ) && !isInRatio ) {
+      this.tonicSoundClip.play();
+      this.timeSinceLastPlay = 0;
+    }
+
+    // if we were in ratio, but now we are not, then fade out the successSoundClip
+    if ( this.isInSuccessfulRatio( this.oldFitness ) && !isInRatio ) {
+      this.successSoundClip.setOutputLevel( 0, .1 );
+      this.playedSuccessYet = false;
+    }
+
+    this.oldFitness = newFitness;
   }
 
   /**
@@ -96,7 +122,7 @@ class StaccatoFrequencySoundGenerator extends SoundGenerator {
    */
   reset() {
     this.tonicSoundClip.stop( 0 );
-    this.thirdSoundClip.stop( 0 );
+    this.successSoundClip.stop( 0 );
     this.timeSinceLastPlay = 0;
   }
 }
