2a6abbd48c68691148909fd6e6c84a3fad46c7d8
zepumph <michael.kauzmann@colorado.edu>
2020-07-02 13:24:55 -0800

    add zoom mag feature, keyStateTracker only to be used for interactionProperty, use reserveForKeyboardDrag, https://github.com/phetsims/ratio-and-proportion/issues/71

--- js/common/view/RatioInteractionListener.js
+++ js/common/view/RatioInteractionListener.js
@@ -29,42 +29,11 @@ class RatioInteractionListener {
     this.targetNode = targetNode;
     this.firstInteractionProperty = firstInteractionProperty;
     this.gridBaseUnitProperty = gridBaseUnitProperty;
+    this.leftValueProperty = leftValueProperty;
+    this.rightValueProperty = rightValueProperty;
 
+    // @private - true whenever the user is interacting with this listener
     this.isBeingInteractedWithProperty = new BooleanProperty( false );
-
-    this.keyStateTracker.keydownEmitter.addListener( event => {
-      this.isBeingInteractedWithProperty.value = true;
-
-      if ( event.key === 'ArrowDown' ) {
-        this.stepValue( rightValueProperty, false );
-      }
-      else if ( event.key === 'ArrowUp' ) {
-        this.stepValue( rightValueProperty, true );
-      }
-      else if ( event.key.toLowerCase() === 'w' ) {
-        this.stepValue( leftValueProperty, true );
-      }
-      else if ( event.key.toLowerCase() === 's' ) {
-        this.stepValue( leftValueProperty, false );
-      }
-      else if ( event.key.toLowerCase() === 'j' ) {
-        this.firstInteractionProperty.value = false;
-
-        // jump both values to the lowest occupied value
-        rightValueProperty.value = leftValueProperty.value = Math.min( leftValueProperty.value, rightValueProperty.value );
-      }
-      else {
-
-        // for number keys 0-9, jump both values to that grid line number. This value changes based on the gridBaseUnitProperty
-        for ( let i = 0; i <= 9; i++ ) {
-          if ( event.key === i + '' ) {
-            this.firstInteractionProperty.value = false;
-            const newValue = 1 / gridBaseUnitProperty.value * i;
-            leftValueProperty.value = rightValueProperty.value = newValue;
-          }
-        }
-      }
-    } );
 
     this.keyStateTracker.keyupEmitter.addListener( () => {
       if ( !this.keyStateTracker.keysAreDown() ) {
@@ -98,8 +67,46 @@ class RatioInteractionListener {
    */
   keydown( sceneryEvent ) {
 
-    // TODO: targetNode is only because there are currently children listeners on each ratio half that we don't want bubbling, https://github.com/phetsims/ratio-and-proportion/issues/44
-    sceneryEvent.target === this.targetNode && this.keyStateTracker.keydownUpdate( sceneryEvent.domEvent );
+    if ( sceneryEvent.target === this.targetNode ) {
+      this.keyStateTracker.keydownUpdate( sceneryEvent.domEvent );
+
+      // signify that this listener is reserved for keyboard movement so that other listeners can change
+      // their behavior during scenery event dispatch
+      sceneryEvent.pointer.reserveForKeyboardDrag();
+
+      this.isBeingInteractedWithProperty.value = true;
+
+      if ( event.key === 'ArrowDown' ) {
+        this.stepValue( this.rightValueProperty, false );
+      }
+      else if ( event.key === 'ArrowUp' ) {
+        this.stepValue( this.rightValueProperty, true );
+      }
+      else if ( event.key.toLowerCase() === 'w' ) {
+        this.stepValue( this.leftValueProperty, true );
+      }
+      else if ( event.key.toLowerCase() === 's' ) {
+        this.stepValue( this.leftValueProperty, false );
+      }
+      else if ( event.key.toLowerCase() === 'j' ) {
+        this.firstInteractionProperty.value = false;
+
+        // jump both values to the lowest occupied value
+        this.rightValueProperty.value = this.leftValueProperty.value = Math.min( this.leftValueProperty.value, this.rightValueProperty.value );
+      }
+      else {
+
+        // for number keys 0-9, jump both values to that grid line number. This value changes based on the gridBaseUnitProperty
+        for ( let i = 0; i <= 9; i++ ) {
+          if ( event.key === i + '' ) {
+            this.firstInteractionProperty.value = false;
+            const newValue = 1 / this.gridBaseUnitProperty.value * i;
+            this.leftValueProperty.value = this.rightValueProperty.value = newValue;
+          }
+        }
+      }
+    }
+
   }
 
   /**
