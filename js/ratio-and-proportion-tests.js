cc90a20925102dfda371c7fc7ee89926483a1b39
zepumph <michael.kauzmann@colorado.edu>
2020-07-20 19:45:43 -0800

    convert fitness algorithm to be based on deltas of left and right values added tests too, https://github.com/phetsims/ratio-and-proportion/issues/91

---
+++ js/ratio-and-proportion-tests.js
@@ -0,0 +1,6 @@
+// Copyright 2020, University of Colorado Boulder
+
+import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
+import './common/model/RatioAndProportionModelTests.js';
+
+qunitStart();
