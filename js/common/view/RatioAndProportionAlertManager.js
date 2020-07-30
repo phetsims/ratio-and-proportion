b3b61f7047f675c2deb461a361a98d3e7946b2f2
zepumph <michael.kauzmann@colorado.edu>
2020-07-30 12:30:08 -0800

    remove movement context responses, https://github.com/phetsims/ratio-and-proportion/issues/108

--- js/common/view/RatioAndProportionAlertManager.js
+++ js/common/view/RatioAndProportionAlertManager.js
@@ -11,15 +11,11 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
-import MovementDescriber from '../../../../scenery-phet/js/accessibility/describers/MovementDescriber.js';
 import ratioAndProportion from '../../ratioAndProportion.js';
 import ratioAndProportionStrings from '../../ratioAndProportionStrings.js';
 
 const leftHandString = ratioAndProportionStrings.a11y.leftHand;
 const rightHandString = ratioAndProportionStrings.a11y.rightHand;
-const firstMovementAlertPatternString = ratioAndProportionStrings.a11y.pointerPosition.firstMovementAlertPattern;
-const movementAlertWithChangePatternString = ratioAndProportionStrings.a11y.pointerPosition.movementAlertWithChangePattern;
 
 class RatioAndProportionAlertManager {
 
@@ -68,86 +64,19 @@ class RatioAndProportionAlertManager {
    */
   alertRatioChange( newValue ) {
     if ( newValue !== this.previouslyDescribedValue ) {
-      let alert = null;
 
       const nextRatioIndex = this.ratioDescriber.getRatioFitnessIndex();
 
-      const changeDescription = this.getChangeInRatioDescription( newValue );
-      const movingUp = newValue > this.previouslyDescribedValue;
-      const directionDescription = MovementDescriber.getDirectionDescriptionFromAngle( movingUp ? -Math.PI / 2 : Math.PI / 2 );
-
-      if ( movingUp && this.firstMovementUp ) {
-        this.firstMovementUp = false;
-        alert = this.getFirstMovementDescription( changeDescription, directionDescription );
+      // if the ratio description changed
+      if ( this.describedRatioIndex !== nextRatioIndex ) {
+        phet.joist.sim.utteranceQueue.addToBack( this.ratioDescriber.getRatioDescriptionString() );
       }
-      else if ( !movingUp && this.firstMovementDown ) {
-        this.firstMovementDown = false;
-        alert = this.getFirstMovementDescription( changeDescription, directionDescription );
-
-      }
-      else {
-
-        // if no other information, describe direction of movement only
-        alert = changeDescription || directionDescription;
-      }
-
-      // Alert if we've got one.
-      alert && phet.joist.sim.utteranceQueue.addToBack( alert );
 
       this.describedRatioIndex = nextRatioIndex;
       this.previouslyDescribedValue = newValue;
     }
   }
 
-  /**
-   * Get the first description of movement for an object. The description for actual movement is more verbose this
-   * first time. After this, movement alerts are shorter until reset. If there was any change in state (ratio, region)
-   * that will be part of this movement description.
-   *
-   * @param {string} changeDescription
-   * @param directionDescription
-   * @returns {string}
-   * @private
-   */
-  getFirstMovementDescription( changeDescription, directionDescription ) {
-    let alert;
-
-    const movementAlert = StringUtils.fillIn( firstMovementAlertPatternString, {
-      object: this.objectString,
-      direction: directionDescription
-    } );
-
-    if ( changeDescription ) {
-      alert = StringUtils.fillIn( movementAlertWithChangePatternString, {
-        movementAlert: movementAlert,
-        change: changeDescription
-      } );
-    }
-    else {
-      alert = movementAlert;
-    }
-
-    return alert;
-  }
-
-  /**
-   * Get a description of changes for the object in response to movement from interaction. Will describe
-   * If none of those have changed since the last description null is returned and no change in state should be described.
-   *
-   * @private
-   * @param {number} newValue
-   * @returns {null|string} - null if there is no change in ratio description
-   */
-  getChangeInRatioDescription( newValue ) {
-    const nextRatioIndex = this.ratioDescriber.getRatioFitnessIndex();
-
-    // if the ratio description changed
-    if ( this.describedRatioIndex !== nextRatioIndex ) {
-      return this.ratioDescriber.getRatioDescriptionString();
-    }
-
-    return null;
-  }
 
   /**
    * Reset state variables that help us describe changes after the end of an interaction.
