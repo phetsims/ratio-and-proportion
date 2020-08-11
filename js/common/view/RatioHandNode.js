2d7d21e2d451ca76ab17293796b9046f33fe5783
zepumph <michael.kauzmann@colorado.edu>
2020-08-11 15:37:58 -0800

    make white circle transparent, https://github.com/phetsims/ratio-and-proportion/issues/122 https://github.com/phetsims/ratio-and-proportion/issues/121

--- js/common/view/RatioHandNode.js
+++ js/common/view/RatioHandNode.js
@@ -10,10 +10,10 @@ import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import Range from '../../../../dot/js/Range.js';
 import merge from '../../../../phet-core/js/merge.js';
-import Circle from '../../../../scenery/js/nodes/Circle.js';
 import Image from '../../../../scenery/js/nodes/Image.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import AccessibleSlider from '../../../../sun/js/accessibility/AccessibleSlider.js';
+import cutOutHandImage from '../../../images/cutout-hand_png.js';
 import filledInHandImage from '../../../images/filled-in-hand_png.js';
 import ratioAndProportion from '../../ratioAndProportion.js';
 import RatioAndProportionConstants from '../RatioAndProportionConstants.js';
@@ -46,22 +46,27 @@ class RatioHandNode extends Node {
     !options.asIcon && this.initializeAccessibleSlider( valueProperty, new Property( valueRange ), new BooleanProperty( true ), options );
 
     // @private
-    const handImage = new Image( filledInHandImage );
-    const handCircle = new Circle( 10, {
-      fill: 'white'
+    const filledInHandNode = new Image( filledInHandImage );
+    const cutOutHandNode = new Image( cutOutHandImage, {
+      visible: false
     } );
 
+    const container = new Node( {
+      children: [ filledInHandNode, cutOutHandNode ],
+      excludeInvisibleChildrenFromBounds: true
+    } );
+    this.addChild( container );
+
     // empirical multipliers to center hand on palm. Don't change these without altering the layout for the cue arrows too.
-    handImage.right = handImage.width * .4;
-    handImage.bottom = handImage.height * .475;
+    container.right = container.width * .365;
+    container.bottom = container.height * .5;
 
     // Only display the "target circles" when the grid is being shown
     gridViewProperty.link( gridView => {
-      handCircle.visible = GridView.displayHorizontal( gridView );
+      const displayCutOut = GridView.displayHorizontal( gridView );
+      cutOutHandNode.visible = displayCutOut;
+      filledInHandNode.visible = !displayCutOut;
     } );
-
-    assert && assert( !options.children, 'RatioHandeNode sets its own children' );
-    this.children = [ handImage, handCircle ];
 
     // Flip the hand if it isn't a right hand. Do this after the circle/hand relative positioning
     this.setScaleMagnitude( ( options.isRight ? 1 : -1 ) * .4, .4 );
