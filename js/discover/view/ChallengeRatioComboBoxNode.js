81a425136d65c65d600d204d2d1d9e141d49aea3
zepumph <michael.kauzmann@colorado.edu>
2020-10-09 11:28:35 -0800

    factor out ComboBox logic into a separate class, https://github.com/phetsims/ratio-and-proportion/issues/57

---
+++ js/discover/view/ChallengeRatioComboBoxNode.js
@@ -0,0 +1,91 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import Node from '../../../../scenery/js/nodes/Node.js';
+import Color from '../../../../scenery/js/util/Color.js';
+import ComboBox from '../../../../sun/js/ComboBox.js';
+import SoundClip from '../../../../tambo/js/sound-generators/SoundClip.js';
+import soundManager from '../../../../tambo/js/soundManager.js';
+import challenge1SelectionSound from '../../../../tambo/sounds/selection-arpeggio-001_mp3.js';
+import challenge2SelectionSound from '../../../../tambo/sounds/selection-arpeggio-004_mp3.js';
+import challenge3SelectionSound from '../../../../tambo/sounds/selection-arpeggio-006_mp3.js';
+import Utterance from '../../../../utterance-queue/js/Utterance.js';
+import ratioAndProportion from '../../ratioAndProportion.js';
+import ratioAndProportionStrings from '../../ratioAndProportionStrings.js';
+import ChallengeComboBoxItem from './ChallengeComboBoxItem.js';
+
+// constants
+const SELECTION_SOUND_OPTIONS = {
+  initialOutputLevel: 0.4
+};
+
+class ChallengeRatioComboBoxNode extends Node {
+
+  /**
+   * @param {NumberProperty} targetRatioProperty
+   * @param {RatioDescriber} ratioDescriber
+   * @param {Object} [options]
+   */
+  constructor( targetRatioProperty, ratioDescriber, options ) {
+
+    assert && options && assert( !options.children, 'ChallengeRatioComboBoxNode sets its own children.' );
+
+    super( options );
+
+    const comboBoxListParent = new Node();
+
+    // sound generators
+    const soundGenerators = [];
+    soundGenerators.push( new SoundClip( challenge1SelectionSound, SELECTION_SOUND_OPTIONS ) );
+    soundGenerators.push( new SoundClip( challenge2SelectionSound, SELECTION_SOUND_OPTIONS ) );
+    soundGenerators.push( new SoundClip( challenge3SelectionSound, SELECTION_SOUND_OPTIONS ) );
+    soundGenerators.forEach( sg => { soundManager.addSoundGenerator( sg ); } );
+
+    // @public (read-only) Used to get the names of challenges based on the target ratio, NOTE: lowercase strings are only available in the PDOM (not yet i18n)
+    this.ratioToChallengeNameMap = new Map();
+    this.ratioToChallengeNameMap.set( 1 / 2, { capitalized: ratioAndProportionStrings.challenge1, lowercase: ratioAndProportionStrings.a11y.discover.challenge1Lowercase } );
+    this.ratioToChallengeNameMap.set( 1 / 3, { capitalized: ratioAndProportionStrings.challenge2, lowercase: ratioAndProportionStrings.a11y.discover.challenge2Lowercase } );
+    this.ratioToChallengeNameMap.set( 3 / 4, { capitalized: ratioAndProportionStrings.challenge3, lowercase: ratioAndProportionStrings.a11y.discover.challenge3Lowercase } );
+
+    const comboBoxHeading = new Node( {
+      innerContent: ratioAndProportionStrings.a11y.discover.ratioChallenges,
+      tagName: 'h3'
+    } );
+    const comboBox = new ComboBox( [
+      new ChallengeComboBoxItem( this.ratioToChallengeNameMap.get( 1 / 2 ).capitalized, new Color( 233, 69, 69 ), 1 / 2, {
+        soundPlayer: soundGenerators[ 0 ],
+        a11yLabel: ratioAndProportionStrings.challenge1
+      } ),
+      new ChallengeComboBoxItem( this.ratioToChallengeNameMap.get( 1 / 3 ).capitalized, new Color( 87, 182, 221 ), 1 / 3, {
+        soundPlayer: soundGenerators[ 1 ],
+        a11yLabel: ratioAndProportionStrings.challenge2
+      } ),
+      new ChallengeComboBoxItem( this.ratioToChallengeNameMap.get( 3 / 4 ).capitalized, new Color( 255, 200, 0 ), 3 / 4, {
+        soundPlayer: soundGenerators[ 2 ],
+        a11yLabel: ratioAndProportionStrings.challenge3
+      } )
+    ], targetRatioProperty, comboBoxListParent, {
+      helpText: ratioAndProportionStrings.a11y.discover.challengesHelpText
+    } );
+
+    const proximityToRatioUtterance = new Utterance();
+    targetRatioProperty.link( () => {
+      proximityToRatioUtterance.alert = ratioDescriber.getProximityToChallengeRatioSentence();
+      phet.joist.sim.utteranceQueue.addToBack( proximityToRatioUtterance );
+    } );
+
+    this.children = [
+      comboBoxHeading,
+      comboBox,
+      comboBoxListParent
+    ];
+
+    this.accessibleOrder = [ comboBoxHeading, comboBox, comboBoxListParent ];
+  }
+}
+
+ratioAndProportion.register( 'ChallengeRatioComboBoxNode', ChallengeRatioComboBoxNode );
+export default ChallengeRatioComboBoxNode;
--- js/discover/view/DiscoverScreenView.js
+++ js/discover/view/DiscoverScreenView.js
@@ -4,25 +4,11 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import Node from '../../../../scenery/js/nodes/Node.js';
-import Color from '../../../../scenery/js/util/Color.js';
-import ComboBox from '../../../../sun/js/ComboBox.js';
-import SoundClip from '../../../../tambo/js/sound-generators/SoundClip.js';
-import soundManager from '../../../../tambo/js/soundManager.js';
-import challenge1SelectionSound from '../../../../tambo/sounds/selection-arpeggio-001_mp3.js';
-import challenge2SelectionSound from '../../../../tambo/sounds/selection-arpeggio-004_mp3.js';
-import challenge3SelectionSound from '../../../../tambo/sounds/selection-arpeggio-006_mp3.js';
-import Utterance from '../../../../utterance-queue/js/Utterance.js';
 import RAPScreenView from '../../common/view/RAPScreenView.js';
 import ratioAndProportion from '../../ratioAndProportion.js';
-import ratioAndProportionStrings from '../../ratioAndProportionStrings.js';
-import ChallengeComboBoxItem from './ChallengeComboBoxItem.js';
+import ChallengeRatioComboBoxNode from './ChallengeRatioComboBoxNode.js';
 import DiscoverScreenSummaryNode from './DiscoverScreenSummaryNode.js';
 
-// constants
-const SELECTION_SOUND_OPTIONS = {
-  initialOutputLevel: 0.4
-};
 
 class DiscoverScreenView extends RAPScreenView {
 
@@ -34,54 +20,11 @@ class DiscoverScreenView extends RAPScreenView {
 
     super( model, tandem );
 
-    const comboBoxListParent = new Node();
+    const comboBoxContainer = new ChallengeRatioComboBoxNode( model.targetRatioProperty, this.ratioDescriber );
 
-    // sound generators
-    const soundGenerators = [];
-    soundGenerators.push( new SoundClip( challenge1SelectionSound, SELECTION_SOUND_OPTIONS ) );
-    soundGenerators.push( new SoundClip( challenge2SelectionSound, SELECTION_SOUND_OPTIONS ) );
-    soundGenerators.push( new SoundClip( challenge3SelectionSound, SELECTION_SOUND_OPTIONS ) );
-    soundGenerators.forEach( sg => { soundManager.addSoundGenerator( sg ); } );
+    this.topScalingUILayerNode.addChild( comboBoxContainer );
 
-    // Used to get the names of challenges based on the target ratio, NOTE: lowercase strings are only available in the PDOM (not yet i18n)
-    const ratioToChallengeNameMap = new Map();
-    ratioToChallengeNameMap.set( 1 / 2, { capitalized: ratioAndProportionStrings.challenge1, lowercase: ratioAndProportionStrings.a11y.discover.challenge1Lowercase } );
-    ratioToChallengeNameMap.set( 1 / 3, { capitalized: ratioAndProportionStrings.challenge2, lowercase: ratioAndProportionStrings.a11y.discover.challenge2Lowercase } );
-    ratioToChallengeNameMap.set( 3 / 4, { capitalized: ratioAndProportionStrings.challenge3, lowercase: ratioAndProportionStrings.a11y.discover.challenge3Lowercase } );
-
-    const comboBoxHeading = new Node( {
-      innerContent: ratioAndProportionStrings.a11y.discover.ratioChallenges,
-      tagName: 'h3'
-    } );
-    const comboBox = new ComboBox( [
-      new ChallengeComboBoxItem( ratioToChallengeNameMap.get( 1 / 2 ).capitalized, new Color( 233, 69, 69 ), 1 / 2, {
-        soundPlayer: soundGenerators[ 0 ],
-        a11yLabel: ratioAndProportionStrings.challenge1
-      } ),
-      new ChallengeComboBoxItem( ratioToChallengeNameMap.get( 1 / 3 ).capitalized, new Color( 87, 182, 221 ), 1 / 3, {
-        soundPlayer: soundGenerators[ 1 ],
-        a11yLabel: ratioAndProportionStrings.challenge2
-      } ),
-      new ChallengeComboBoxItem( ratioToChallengeNameMap.get( 3 / 4 ).capitalized, new Color( 255, 200, 0 ), 3 / 4, {
-        soundPlayer: soundGenerators[ 2 ],
-        a11yLabel: ratioAndProportionStrings.challenge3
-      } )
-    ], model.targetRatioProperty, comboBoxListParent, {
-      helpText: ratioAndProportionStrings.a11y.discover.challengesHelpText
-    } );
-
-    const proximityToRatioUtterance = new Utterance();
-    model.targetRatioProperty.link( () => {
-      proximityToRatioUtterance.alert = this.ratioDescriber.getProximityToChallengeRatioSentence();
-      phet.joist.sim.utteranceQueue.addToBack( proximityToRatioUtterance );
-    } );
-
-    // children - scale all of these components to support vertical aspect ratios better.
-    this.topScalingUILayerNode.addChild( comboBoxHeading );
-    this.topScalingUILayerNode.addChild( comboBox );
-    this.topScalingUILayerNode.addChild( comboBoxListParent );
-
-    this.pdomPlayAreaNode.accessibleOrder = this.pdomPlayAreaNode.accessibleOrder.concat( [ comboBoxHeading, comboBox, comboBoxListParent ] );
+    this.pdomPlayAreaNode.accessibleOrder = this.pdomPlayAreaNode.accessibleOrder.concat( [ comboBoxContainer ] );
 
     // set this after the supertype has initialized the view code needed to create the screen summary
     this.setScreenSummaryContent( new DiscoverScreenSummaryNode(
@@ -92,12 +35,12 @@ class DiscoverScreenView extends RAPScreenView {
       this.tickMarkViewProperty,
       this.ratioDescriber,
       this.handPositionsDescriber,
-      ratioToChallengeNameMap
+      comboBoxContainer.ratioToChallengeNameMap
     ) );
 
     // layout
-    comboBox.left = this.tickMarkViewRadioButtonGroup.left;
-    comboBox.top = this.tickMarkViewRadioButtonGroup.bottom + 20;
+    comboBoxContainer.left = this.tickMarkViewRadioButtonGroup.left;
+    comboBoxContainer.top = this.tickMarkViewRadioButtonGroup.bottom + 20;
   }
 }
 
