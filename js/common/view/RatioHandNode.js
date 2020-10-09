424f6cb4fcdd7c4a466137ebaf4b7940ce671d0e
zepumph <michael.kauzmann@colorado.edu>
2020-10-09 10:54:48 -0800

    convert hands to be SCENERY/Path shapes instead of pngs. https://github.com/phetsims/ratio-and-proportion/issues/185

--- js/common/view/RatioHandNode.js
+++ js/common/view/RatioHandNode.js
@@ -11,14 +11,15 @@ import Property from '../../../../axon/js/Property.js';
 import Range from '../../../../dot/js/Range.js';
 import merge from '../../../../phet-core/js/merge.js';
 import Orientation from '../../../../phet-core/js/Orientation.js';
-import Image from '../../../../scenery/js/nodes/Image.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
+import Path from '../../../../scenery/js/nodes/Path.js';
 import AccessibleSlider from '../../../../sun/js/accessibility/AccessibleSlider.js';
-import cutOutHandImage from '../../../images/cutout-hand_png.js';
-import filledInHandImage from '../../../images/filled-in-hand_png.js';
 import ratioAndProportion from '../../ratioAndProportion.js';
 import RAPConstants from '../RAPConstants.js';
 import TickMarkView from './TickMarkView.js';
+
+// constants
+const HAND_PATH_SCALE = 2;
 
 class RatioHandNode extends Node {
 
@@ -48,8 +49,8 @@ class RatioHandNode extends Node {
     !options.asIcon && this.initializeAccessibleSlider( valueProperty, enabledRatioComponentsRangeProperty, new BooleanProperty( true ), options );
 
     // @private
-    const filledInHandNode = new Image( filledInHandImage );
-    const cutOutHandNode = new Image( cutOutHandImage, {
+    const filledInHandNode = new FilledInHandPath();
+    const cutOutHandNode = new CutOutHandPath( {
       visible: false
     } );
 
@@ -61,7 +62,7 @@ class RatioHandNode extends Node {
 
     // empirical multipliers to center hand on palm. Don't change these without altering the layout for the cue arrows too.
     container.right = container.width * .365;
-    container.bottom = container.height * .5;
+    container.bottom = container.height * .54;
 
     // Only display the "target circles" when the tick marks are being shown
     tickMarkViewProperty.link( tickMarkView => {
@@ -94,6 +95,59 @@ class RatioHandNode extends Node {
         pickable: false
       }, options ) ) ]
     } );
+  }
+}
+
+class FilledInHandPath extends Path {
+
+  /**
+   * @param {Object} [options]
+   */
+  constructor( options ) {
+
+    options = merge( {
+      fill: 'black',
+      scale: HAND_PATH_SCALE
+    }, options );
+
+    const shape = `M27.654,1.738c-0.716-0.716-1.572-1.075-2.57-1.075c-1,0-1.856,0.359-2.572,1.075c-0.717,0.717-1.074,1.573-1.074,2.571
+v21.354h-1.041V8.477c0-0.998-0.359-1.856-1.075-2.572c-0.717-0.715-1.573-1.073-2.571-1.073c-0.999,0-1.856,0.358-2.572,1.073
+c-0.715,0.717-1.073,1.574-1.073,2.572v17.188v8.334l-5.013-6.674c-0.825-1.106-1.933-1.66-3.321-1.66
+c-1.15,0-2.132,0.406-2.945,1.221c-0.814,0.813-1.222,1.796-1.222,2.945c0,0.934,0.282,1.769,0.847,2.507l12.5,16.667
+c0.825,1.107,2.96,3.442,6.249,4.425c7.18,2.147,11.243,1.665,15.685,0.211c3.871-1.267,4.833-2.464,6.875-5.514
+c1.932-2.887,2.112-9.526,2.475-13.186c0.069-0.698,0.162-1.334,0.162-1.92v-16.21c0-0.998-0.358-1.856-1.073-2.572
+c-0.717-0.717-1.574-1.073-2.572-1.073c-0.998,0-1.855,0.357-2.571,1.073c-0.717,0.716-1.075,1.574-1.075,2.572v8.854h-1.042V8.477
+c0-0.998-0.356-1.856-1.073-2.572c-0.717-0.715-1.574-1.073-2.572-1.073c-0.998,0-1.854,0.358-2.571,1.073
+c-0.716,0.717-1.074,1.574-1.074,2.572v17.188h-1.041V4.31C28.729,3.311,28.371,2.455,27.654,1.738z`;
+
+    super( shape, options );
+  }
+}
+
+class CutOutHandPath extends Path {
+
+  /**
+   * @param {Object} [options]
+   */
+  constructor( options ) {
+
+    options = merge( {
+      fill: 'black',
+      scale: HAND_PATH_SCALE
+    }, options );
+
+    const shape = `M44.323,14.238c-0.717-0.717-1.574-1.073-2.572-1.073s-1.855,0.357-2.571,1.073c-0.717,0.716-1.075,1.574-1.075,2.572v8.854
+h-1.042V8.477c0-0.998-0.356-1.856-1.073-2.573c-0.717-0.715-1.574-1.073-2.572-1.073s-1.854,0.358-2.571,1.073
+c-0.716,0.717-1.074,1.574-1.074,2.573v12.239c2.514,0.263,4.481,2.364,4.481,4.948c0,2.761-2.239,5-5,5s-5-2.239-5-5
+c0-2.583,1.965-4.683,4.477-4.948V4.31c0-0.998-0.358-1.854-1.075-2.571c-0.716-0.716-1.572-1.075-2.57-1.075
+c-1,0-1.856,0.359-2.572,1.075c-0.717,0.717-1.074,1.573-1.074,2.571v21.354h-1.041V8.477c0-0.998-0.359-1.856-1.075-2.573
+c-0.717-0.715-1.573-1.073-2.571-1.073c-0.999,0-1.855,0.358-2.572,1.073c-0.715,0.717-1.073,1.574-1.073,2.573v17.188v8.334
+l-5.013-6.674c-0.825-1.106-1.933-1.66-3.321-1.66c-1.15,0-2.132,0.406-2.946,1.221c-0.814,0.813-1.221,1.796-1.221,2.945
+c0,0.934,0.282,1.769,0.846,2.507l12.5,16.667c0.825,1.107,2.96,3.442,6.249,4.425c7.18,2.147,11.243,1.665,15.685,0.211
+c3.871-1.267,4.833-2.463,6.875-5.514c1.932-2.887,2.112-9.526,2.475-13.186c0.069-0.698,0.162-1.334,0.162-1.92v-16.21
+C45.396,15.812,45.038,14.954,44.323,14.238z`;
+
+    super( shape, options );
   }
 }
 
