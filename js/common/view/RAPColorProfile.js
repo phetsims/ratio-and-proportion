f8821e99785f75b40fe9c9fb27b73859fb05bfc4
samreid <reids@colorado.edu>
2021-07-18 13:29:48 -0600

    Use ProfileColorProperty, see https://github.com/phetsims/scenery-phet/issues/515

--- js/common/view/RAPColorProfile.js
+++ js/common/view/RAPColorProfile.js
@@ -6,43 +6,49 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import ColorProfile from '../../../../scenery-phet/js/ColorProfile.js';
 import Color from '../../../../scenery/js/util/Color.js';
+import ProfileColorProperty from '../../../../scenery/js/util/ProfileColorProperty.js';
 import ratioAndProportion from '../../ratioAndProportion.js';
 
 // Even though there is only one Profile, it is still nice to use this pattern for color organizing.
-const RAPColorProfile = new ColorProfile( [ 'default' ], {
-  tickMarksAndLabelsInFitness: {
+const RAPColorProfile = {
+  tickMarksAndLabelsInFitnessProperty: new ProfileColorProperty( 'tickMarksAndLabelsInFitness', {
     default: Color.DARK_GRAY
-  },
-  tickMarksAndLabelsOutOfFitness: {
+  } ),
+  tickMarksAndLabelsOutOfFitnessProperty: new ProfileColorProperty( 'tickMarksAndLabelsOutOfFitness', {
     default: Color.GRAY
-  },
-  backgroundInFitness: { // the color will jump from backgroundInterpolationToFitness to this when actually in ratio
+  } ),
+
+  // the color will jump from backgroundInterpolationToFitness to this when actually in ratio
+  backgroundInFitnessProperty: new ProfileColorProperty( 'backgroundInFitness', {
     default: new Color( '#5ab46c' )
-  },
-  backgroundInterpolationToFitness: { // this will be the max of the interpolation for the background color
+  } ),
+
+  // this will be the max of the interpolation for the background color
+  backgroundInterpolationToFitnessProperty: new ProfileColorProperty( 'backgroundInterpolationToFitness', {
     default: new Color( '#77ce81' )
-  },
-  backgroundOutOfFitness: {
+  } ),
+  backgroundOutOfFitnessProperty: new ProfileColorProperty( 'backgroundOutOfFitness', {
     default: new Color( 'white' )
-  },
-  cueArrows: { // cue arrows around the ratio hands.
+  } ),
+
+  // cue arrows around the ratio hands.
+  cueArrowsProperty: new ProfileColorProperty( 'cueArrows', {
     default: Color.DARK_GRAY
-  },
-  createScreenHand: {
+  } ),
+  createScreenHandProperty: new ProfileColorProperty( 'createScreenHand', {
     default: new Color( '#8d5cbd' )
-  },
-  discoverChallenge1: {
+  } ),
+  discoverChallenge1Property: new ProfileColorProperty( 'discoverChallenge1', {
     default: new Color( 233, 69, 69 )
-  },
-  discoverChallenge2: {
+  } ),
+  discoverChallenge2Property: new ProfileColorProperty( 'discoverChallenge2', {
     default: new Color( 87, 182, 221 )
-  },
-  discoverChallenge3: {
+  } ),
+  discoverChallenge3Property: new ProfileColorProperty( 'discoverChallenge3', {
     default: new Color( 255, 200, 0 )
-  }
-} );
+  } )
+};
 
 ratioAndProportion.register( 'RAPColorProfile', RAPColorProfile );
 export default RAPColorProfile;
