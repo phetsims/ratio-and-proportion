cb97dadd81e22cf22f43eff7b9d5b3e8d79cc465
Sam Reid <reids@colorado.edu>
2021-11-11 22:37:00 -0700

    Regenerate string file with grunt modulify, see https://github.com/phetsims/chipper/issues/1139

--- js/ratioAndProportionStrings.ts
+++ js/ratioAndProportionStrings.ts
@@ -3,12 +3,236 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import ratioAndProportion from './ratioAndProportion.js';
 
-// TODO: figure out getStringModule pattern, https://github.com/phetsims/ratio-and-proportion/issues/404
-const ratioAndProportionStrings = getStringModule( 'RATIO_AND_PROPORTION' ) as any;
+type StringsType = {
+  'ratio-and-proportion': {
+    'title': string
+  },
+  'screen': {
+    'discover': string,
+    'create': string
+  },
+  'myChallenge': string,
+  'range': string,
+  'zeroToTen': string,
+  'zeroToTwenty': string,
+  'zeroToThirty': string,
+  'challenge1': string,
+  'challenge2': string,
+  'challenge3': string,
+  'lockRatio': string,
+  'moveBothHandsSimultaneously': string,
+  'moveLeftHand': string,
+  'moveRightHand': string,
+  'moveHandsInSmallerSteps': string,
+  'jumpBothHands': string,
+  'setMyRatioChallenge': string,
+  'setHandRatioValue': string,
+  'jumpToMinimum': string,
+  'jumpToMaximum': string,
+  'moveHandsIndividually': string,
+  'move': string,
+  'leftOrRightHand': string,
+  'top': string,
+  'bottom': string,
+  'challengeRatio': string,
+  'a11y': {
+    'keyboardHelp': {
+      'leftHandDescription': string,
+      'rightHandDescription': string,
+      'handsInSmallerStepsDescription': string,
+      'jumpBothHandsDescription': string,
+      'setHandRatioValueDescription': string,
+      'jumpToMinimumDescription': string,
+      'jumpToMaximumDescription': string
+    },
+    'leftHand': string,
+    'rightHand': string,
+    'leftHandLower': string,
+    'rightHandLower': string,
+    'leftValue': string,
+    'rightValue': string,
+    'individualHandsHelpText': string,
+    'bothHands': {
+      'bothHands': string,
+      'bothHandsHelpText': string,
+      'handsDistancePattern': string,
+      'handsDistancePatternCapitalized': string,
+      'handsDistanceProgressPattern': string,
+      'equalObjectResponseAlert': string,
+      'eachObjectResponseAlert': string,
+      'cannotJumpToZeroWhenLocked': string
+    },
+    'tickMark': {
+      'heading': string,
+      'showNo': string,
+      'show': string,
+      'showNumbered': string,
+      'tickMarksHidden': string,
+      'tickMarksShown': string,
+      'numberedTickMarksShown': string,
+      'relative': {
+        'around': string,
+        'aroundHalfwayPast': string,
+        'almostHalfwayPast': string,
+        'halfwayPast': string,
+        'halfwayTo': string,
+        'aroundHalfwayTo': string,
+        'almostHalfwayTo': string,
+        'almostOn': string,
+        'on': string,
+        'at': string,
+        'near': string,
+        'zero': string
+      },
+      'semiQuantitativeHandPositionPattern': string,
+      'quantitativeHandPositionPattern': string,
+      'ordinal': {
+        'first': string,
+        'second': string,
+        'third': string,
+        'fourth': string,
+        'fifth': string,
+        'sixth': string,
+        'seventh': string,
+        'eighth': string,
+        'ninth': string,
+        'tenth': string,
+        'eleventh': string,
+        'twelfth': string,
+        'thirteenth': string,
+        'fourteenth': string,
+        'fifteenth': string,
+        'sixteenth': string,
+        'seventeenth': string,
+        'eighteenth': string,
+        'nineteenth': string,
+        'twentieth': string,
+        'twentyFirst': string,
+        'twentySecond': string,
+        'twentyThird': string,
+        'twentyFourth': string,
+        'twentyFifth': string,
+        'twentySixth': string,
+        'twentySeventh': string,
+        'twentyEighth': string,
+        'twentyNinth': string
+      }
+    },
+    'leftHandBullet': string,
+    'rightHandBullet': string,
+    'ofPlayAreaPattern': string,
+    'lockRatioHelpText': string,
+    'lockRatioCheckboxContextResponse': string,
+    'ratioNoLongerLocked': string,
+    'screenSummaryControlAreaParagraph': string,
+    'discover': {
+      'homeScreenDescription': string,
+      'screenSummary': {
+        'paragraph1': string,
+        'paragraph2': string,
+        'interactionHint': string,
+        'qualitativeStateOfSim': string
+      },
+      'challengesHelpText': string,
+      'challenge1Lowercase': string,
+      'challenge2Lowercase': string,
+      'challenge3Lowercase': string,
+      'bothHandsGestureDescriptionHelpText': string
+    },
+    'create': {
+      'homeScreenDescription': string,
+      'screenSummary': {
+        'paragraph1': string,
+        'paragraph2': string,
+        'interactionHint': string,
+        'qualitativeStateOfSim': string
+      },
+      'tickMarkRangeHelpText': string,
+      'tickMarkRangeContextResponse': string,
+      'myChallengeHelpText': string,
+      'bothHandsGestureDescriptionHelpText': string
+    },
+    'handPosition': {
+      'atTop': string,
+      'nearTop': string,
+      'inUpperRegion': string,
+      'inUpperMiddleRegion': string,
+      'atMiddle': string,
+      'inLowerMiddleRegion': string,
+      'inLowerRegion': string,
+      'nearBottom': string,
+      'atBottom': string,
+      'distance': {
+        'capitalized': {
+          'farthestFrom': string,
+          'extremelyFarFrom': string,
+          'veryFarFrom': string,
+          'farFrom': string,
+          'notSoCloseTo': string,
+          'somewhatCloseTo': string,
+          'veryCloseTo': string,
+          'extremelyCloseTo': string,
+          'almostEvenWith': string,
+          'evenWith': string
+        },
+        'lowercase': {
+          'farthestFrom': string,
+          'extremelyFarFrom': string,
+          'veryFarFrom': string,
+          'farFrom': string,
+          'notSoCloseTo': string,
+          'somewhatCloseTo': string,
+          'veryCloseTo': string,
+          'extremelyCloseTo': string,
+          'almostEvenWith': string,
+          'evenWith': string
+        }
+      },
+      'closerTogether': string,
+      'fartherApart': string,
+      'closerTo': string,
+      'fartherFrom': string,
+      'distanceOrDistanceProgressClause': string
+    },
+    'ratio': {
+      'distancePositionContextResponse': string,
+      'ratioLockedEdgeContextResponse': string,
+      'proximityToNewRatioPattern': string,
+      'proximityToRatioObjectResponse': string,
+      'targetRatioChangedContextResponse': string,
+      'currentChallenge': string,
+      'currentChallengeHidden': string,
+      'capitalized': {
+        'at': string,
+        'extremelyCloseTo': string,
+        'veryCloseTo': string,
+        'somewhatCloseTo': string,
+        'notSoCloseTo': string,
+        'farFrom': string,
+        'veryFarFrom': string,
+        'extremelyFarFrom': string
+      },
+      'lowercase': {
+        'at': string,
+        'extremelyCloseTo': string,
+        'veryCloseTo': string,
+        'somewhatCloseTo': string,
+        'notSoCloseTo': string,
+        'farFrom': string,
+        'veryFarFrom': string,
+        'extremelyFarFrom': string
+      }
+    },
+    'down': string,
+    'up': string
+  }
+};
+
+const ratioAndProportionStrings = getStringModule( 'RATIO_AND_PROPORTION' ) as StringsType;
 
 ratioAndProportion.register( 'ratioAndProportionStrings', ratioAndProportionStrings );
 
