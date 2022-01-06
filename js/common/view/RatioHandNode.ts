4cbeef13effd07eb34d62af933f192294860be34
phet-dev <phetadmin@gmail.com>
2022-01-06 03:20:27 -0700

    update copyright dates from daily grunt work

--- js/common/view/RatioHandNode.ts
+++ js/common/view/RatioHandNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * Hand that marks the location of half of the ratio. This supports alternative input and description for controlling this ratio
