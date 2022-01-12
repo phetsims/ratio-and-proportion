b0d014d88e31bc2da6786780aa97aec25e9e2897
phet-dev <phetadmin@gmail.com>
2022-01-12 03:28:36 -0700

    update copyright dates from daily grunt work

--- js/common/view/BothHandsPDOMNode.ts
+++ js/common/view/BothHandsPDOMNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * PDOM view for interacting with both hands at the same time. This adds a custom interaction, as well as PDOM
