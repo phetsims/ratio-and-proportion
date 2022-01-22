7807dc0512878a1065124b9a5c1af98d5f33d5e9
phet-dev <phetadmin@gmail.com>
2022-01-22 03:24:01 -0700

    update copyright dates from daily grunt work

--- js/common/view/describers/HandPositionsDescriber.ts
+++ js/common/view/describers/HandPositionsDescriber.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * Description for the positions of each hand, as well as their positional relationship like the distance between each
