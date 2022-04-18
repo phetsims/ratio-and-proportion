c62644e003c8773533658706c0a91185a85902e5
zepumph <michael.kauzmann@colorado.edu>
2022-04-18 17:11:36 -0600

    Add enum for this work,  https://github.com/phetsims/ratio-and-proportion/issues/457

---
+++ js/common/view/describers/RatioInputModality.ts
@@ -0,0 +1,20 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * Data type that holds how the ratio can be interacted with
+ *
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import Enumeration from '../../../../../phet-core/js/Enumeration.js';
+import RatioTerm from '../../model/RatioTerm.js';
+
+class RatioInputModality extends RatioTerm {
+  static BOTH_HANDS = new RatioInputModality();
+
+  static override enumeration = new Enumeration( RatioInputModality, {
+    instanceType: RatioTerm
+  } );
+}
+
+export default RatioInputModality;
