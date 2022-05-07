d5730599aec919ca313f9aacefe85b5ecef0f388
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:09 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/ratioAndProportionStrings.ts
+++ js/ratioAndProportionStrings.ts
@@ -9,246 +9,246 @@ import ratioAndProportion from './ratioAndProportion.js';
 
 type StringsType = {
   'ratio-and-proportion': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'discover': string,
-    'create': string
-  },
-  'myChallenge': string,
-  'range': string,
-  'zeroToTen': string,
-  'zeroToTwenty': string,
-  'zeroToThirty': string,
-  'challenge1': string,
-  'challenge2': string,
-  'challenge3': string,
-  'ratioLock': string,
-  'moveBothHandsSimultaneously': string,
-  'moveLeftHand': string,
-  'moveRightHand': string,
-  'moveHandsInSmallerSteps': string,
-  'jumpBothHands': string,
-  'setMyRatioChallenge': string,
-  'setHandRatioValue': string,
-  'jumpToMinimum': string,
-  'jumpToMaximum': string,
-  'moveHandsIndividually': string,
-  'move': string,
-  'leftOrRightHand': string,
-  'top': string,
-  'bottom': string,
-  'challengeRatio': string,
-  'chooseAChallengeRatio': string,
+    'discover': string;
+    'create': string;
+  };
+  'myChallenge': string;
+  'range': string;
+  'zeroToTen': string;
+  'zeroToTwenty': string;
+  'zeroToThirty': string;
+  'challenge1': string;
+  'challenge2': string;
+  'challenge3': string;
+  'ratioLock': string;
+  'moveBothHandsSimultaneously': string;
+  'moveLeftHand': string;
+  'moveRightHand': string;
+  'moveHandsInSmallerSteps': string;
+  'jumpBothHands': string;
+  'setMyRatioChallenge': string;
+  'setHandRatioValue': string;
+  'jumpToMinimum': string;
+  'jumpToMaximum': string;
+  'moveHandsIndividually': string;
+  'move': string;
+  'leftOrRightHand': string;
+  'top': string;
+  'bottom': string;
+  'challengeRatio': string;
+  'chooseAChallengeRatio': string;
   'a11y': {
     'keyboardHelp': {
-      'leftHandDescription': string,
-      'rightHandDescription': string,
-      'handsInSmallerStepsDescription': string,
-      'jumpBothHandsDescription': string,
-      'setHandRatioValueDescription': string,
-      'jumpToMinimumDescription': string,
-      'jumpToMaximumDescription': string
-    },
-    'leftHand': string,
-    'rightHand': string,
-    'leftHandLower': string,
-    'rightHandLower': string,
-    'leftValue': string,
-    'rightValue': string,
-    'individualHandsHelpText': string,
-    'individualHandsVoicingHelpText': string,
+      'leftHandDescription': string;
+      'rightHandDescription': string;
+      'handsInSmallerStepsDescription': string;
+      'jumpBothHandsDescription': string;
+      'setHandRatioValueDescription': string;
+      'jumpToMinimumDescription': string;
+      'jumpToMaximumDescription': string;
+    };
+    'leftHand': string;
+    'rightHand': string;
+    'leftHandLower': string;
+    'rightHandLower': string;
+    'leftValue': string;
+    'rightValue': string;
+    'individualHandsHelpText': string;
+    'individualHandsVoicingHelpText': string;
     'bothHands': {
-      'bothHands': string,
-      'bothHandsHelpText': string,
-      'handsDistancePattern': string,
-      'handsDistancePatternCapitalized': string,
-      'handsDistanceProgressPattern': string,
-      'equalObjectResponseAlert': string,
-      'eachObjectResponseAlert': string,
-      'cannotJumpToZeroWhenLocked': string
-    },
+      'bothHands': string;
+      'bothHandsHelpText': string;
+      'handsDistancePattern': string;
+      'handsDistancePatternCapitalized': string;
+      'handsDistanceProgressPattern': string;
+      'equalObjectResponseAlert': string;
+      'eachObjectResponseAlert': string;
+      'cannotJumpToZeroWhenLocked': string;
+    };
     'tickMark': {
-      'heading': string,
-      'showNo': string,
-      'show': string,
-      'showNumbered': string,
-      'tickMarksHidden': string,
-      'tickMarksShown': string,
-      'numberedTickMarksShown': string,
+      'heading': string;
+      'showNo': string;
+      'show': string;
+      'showNumbered': string;
+      'tickMarksHidden': string;
+      'tickMarksShown': string;
+      'numberedTickMarksShown': string;
       'relative': {
-        'around': string,
-        'aroundHalfwayPast': string,
-        'almostHalfwayPast': string,
-        'halfwayPast': string,
-        'halfwayTo': string,
-        'aroundHalfwayTo': string,
-        'almostHalfwayTo': string,
-        'almostOn': string,
-        'on': string,
-        'at': string,
-        'near': string,
-        'zero': string
-      },
-      'semiQuantitativeHandPositionPattern': string,
-      'quantitativeHandPositionPattern': string,
+        'around': string;
+        'aroundHalfwayPast': string;
+        'almostHalfwayPast': string;
+        'halfwayPast': string;
+        'halfwayTo': string;
+        'aroundHalfwayTo': string;
+        'almostHalfwayTo': string;
+        'almostOn': string;
+        'on': string;
+        'at': string;
+        'near': string;
+        'zero': string;
+      };
+      'semiQuantitativeHandPositionPattern': string;
+      'quantitativeHandPositionPattern': string;
       'ordinal': {
-        'first': string,
-        'second': string,
-        'third': string,
-        'fourth': string,
-        'fifth': string,
-        'sixth': string,
-        'seventh': string,
-        'eighth': string,
-        'ninth': string,
-        'tenth': string,
-        'eleventh': string,
-        'twelfth': string,
-        'thirteenth': string,
-        'fourteenth': string,
-        'fifteenth': string,
-        'sixteenth': string,
-        'seventeenth': string,
-        'eighteenth': string,
-        'nineteenth': string,
-        'twentieth': string,
-        'twentyFirst': string,
-        'twentySecond': string,
-        'twentyThird': string,
-        'twentyFourth': string,
-        'twentyFifth': string,
-        'twentySixth': string,
-        'twentySeventh': string,
-        'twentyEighth': string,
-        'twentyNinth': string
+        'first': string;
+        'second': string;
+        'third': string;
+        'fourth': string;
+        'fifth': string;
+        'sixth': string;
+        'seventh': string;
+        'eighth': string;
+        'ninth': string;
+        'tenth': string;
+        'eleventh': string;
+        'twelfth': string;
+        'thirteenth': string;
+        'fourteenth': string;
+        'fifteenth': string;
+        'sixteenth': string;
+        'seventeenth': string;
+        'eighteenth': string;
+        'nineteenth': string;
+        'twentieth': string;
+        'twentyFirst': string;
+        'twentySecond': string;
+        'twentyThird': string;
+        'twentyFourth': string;
+        'twentyFifth': string;
+        'twentySixth': string;
+        'twentySeventh': string;
+        'twentyEighth': string;
+        'twentyNinth': string;
       }
-    },
-    'leftHandBullet': string,
-    'rightHandBullet': string,
-    'ratioLockEnabledHelpText': string,
-    'ratioLockDisabledHelpText': string,
-    'ratioLockCheckboxContextResponse': string,
-    'ratioNoLongerLocked': string,
-    'ratioNoLongerLockedPattern': string,
-    'screenSummaryControlAreaParagraph': string,
-    'screenSummaryQualitativeStateOfSim': string,
-    'detailsButtonPattern': string,
-    'detailsButtonWithCurrentChallengePattern': string,
+    };
+    'leftHandBullet': string;
+    'rightHandBullet': string;
+    'ratioLockEnabledHelpText': string;
+    'ratioLockDisabledHelpText': string;
+    'ratioLockCheckboxContextResponse': string;
+    'ratioNoLongerLocked': string;
+    'ratioNoLongerLockedPattern': string;
+    'screenSummaryControlAreaParagraph': string;
+    'screenSummaryQualitativeStateOfSim': string;
+    'detailsButtonPattern': string;
+    'detailsButtonWithCurrentChallengePattern': string;
     'discover': {
-      'homeScreenDescription': string,
+      'homeScreenDescription': string;
       'screenSummary': {
-        'paragraph1': string,
-        'paragraph2': string,
-        'paragraph3': string,
-        'interactionHint': string
-      },
-      'overviewSentence': string,
-      'challengesHelpText': string,
-      'challenge1Lowercase': string,
-      'challenge2Lowercase': string,
-      'challenge3Lowercase': string,
-      'bothHandsGestureDescriptionHelpText': string
-    },
+        'paragraph1': string;
+        'paragraph2': string;
+        'paragraph3': string;
+        'interactionHint': string;
+      };
+      'overviewSentence': string;
+      'challengesHelpText': string;
+      'challenge1Lowercase': string;
+      'challenge2Lowercase': string;
+      'challenge3Lowercase': string;
+      'bothHandsGestureDescriptionHelpText': string;
+    };
     'create': {
-      'homeScreenDescription': string,
+      'homeScreenDescription': string;
       'screenSummary': {
-        'paragraph1': string,
-        'paragraph2': string,
-        'interactionHint': string
-      },
-      'overviewSentence': string,
-      'challenge': string,
-      'tickMarkRangeHelpText': string,
-      'tickMarkRangeContextResponse': string,
-      'myChallengeHelpText': string,
-      'myChallengeHintText': string,
-      'myChallengeReadingBlockHintText': string,
-      'bothHandsGestureDescriptionHelpText': string,
-      'rangeLabelPattern': string
-    },
+        'paragraph1': string;
+        'paragraph2': string;
+        'interactionHint': string;
+      };
+      'overviewSentence': string;
+      'challenge': string;
+      'tickMarkRangeHelpText': string;
+      'tickMarkRangeContextResponse': string;
+      'myChallengeHelpText': string;
+      'myChallengeHintText': string;
+      'myChallengeReadingBlockHintText': string;
+      'bothHandsGestureDescriptionHelpText': string;
+      'rangeLabelPattern': string;
+    };
     'handPosition': {
-      'atTop': string,
-      'nearTop': string,
-      'inUpperRegion': string,
-      'inUpperMiddleRegion': string,
-      'atMiddle': string,
-      'aroundMiddle': string,
-      'inLowerMiddleRegion': string,
-      'inLowerRegion': string,
-      'nearBottom': string,
-      'atBottom': string,
+      'atTop': string;
+      'nearTop': string;
+      'inUpperRegion': string;
+      'inUpperMiddleRegion': string;
+      'atMiddle': string;
+      'aroundMiddle': string;
+      'inLowerMiddleRegion': string;
+      'inLowerRegion': string;
+      'nearBottom': string;
+      'atBottom': string;
       'distance': {
         'capitalized': {
-          'farthestFrom': string,
-          'extremelyFarFrom': string,
-          'veryFarFrom': string,
-          'farFrom': string,
-          'notSoCloseTo': string,
-          'somewhatCloseTo': string,
-          'veryCloseTo': string,
-          'extremelyCloseTo': string,
-          'almostEvenWith': string,
-          'evenWith': string
-        },
+          'farthestFrom': string;
+          'extremelyFarFrom': string;
+          'veryFarFrom': string;
+          'farFrom': string;
+          'notSoCloseTo': string;
+          'somewhatCloseTo': string;
+          'veryCloseTo': string;
+          'extremelyCloseTo': string;
+          'almostEvenWith': string;
+          'evenWith': string;
+        };
         'lowercase': {
-          'farthestFrom': string,
-          'extremelyFarFrom': string,
-          'veryFarFrom': string,
-          'farFrom': string,
-          'notSoCloseTo': string,
-          'somewhatCloseTo': string,
-          'veryCloseTo': string,
-          'extremelyCloseTo': string,
-          'almostEvenWith': string,
-          'evenWith': string
+          'farthestFrom': string;
+          'extremelyFarFrom': string;
+          'veryFarFrom': string;
+          'farFrom': string;
+          'notSoCloseTo': string;
+          'somewhatCloseTo': string;
+          'veryCloseTo': string;
+          'extremelyCloseTo': string;
+          'almostEvenWith': string;
+          'evenWith': string;
         }
-      },
-      'closerTogether': string,
-      'fartherApart': string,
-      'closerTo': string,
-      'fartherFrom': string,
-      'distanceOrDistanceProgressClause': string
-    },
+      };
+      'closerTogether': string;
+      'fartherApart': string;
+      'closerTo': string;
+      'fartherFrom': string;
+      'distanceOrDistanceProgressClause': string;
+    };
     'ratio': {
-      'distancePositionContextResponse': string,
-      'bothHandsGoBeyondEdgeContextResponse': string,
-      'singleHandGoBeyondEdgeContextResponse': string,
-      'proximityToNewRatioPattern': string,
-      'proximityToRatioObjectResponse': string,
-      'targetRatioChangedContextResponse': string,
-      'currentChallenge': string,
-      'currentChallengeHidden': string,
+      'distancePositionContextResponse': string;
+      'bothHandsGoBeyondEdgeContextResponse': string;
+      'singleHandGoBeyondEdgeContextResponse': string;
+      'proximityToNewRatioPattern': string;
+      'proximityToRatioObjectResponse': string;
+      'targetRatioChangedContextResponse': string;
+      'currentChallenge': string;
+      'currentChallengeHidden': string;
       'capitalized': {
-        'at': string,
-        'extremelyCloseTo': string,
-        'veryCloseTo': string,
-        'somewhatCloseTo': string,
-        'notSoCloseTo': string,
-        'farFrom': string,
-        'veryFarFrom': string,
-        'extremelyFarFrom': string
-      },
+        'at': string;
+        'extremelyCloseTo': string;
+        'veryCloseTo': string;
+        'somewhatCloseTo': string;
+        'notSoCloseTo': string;
+        'farFrom': string;
+        'veryFarFrom': string;
+        'extremelyFarFrom': string;
+      };
       'lowercase': {
-        'at': string,
-        'extremelyCloseTo': string,
-        'veryCloseTo': string,
-        'somewhatCloseTo': string,
-        'notSoCloseTo': string,
-        'farFrom': string,
-        'veryFarFrom': string,
-        'extremelyFarFrom': string
+        'at': string;
+        'extremelyCloseTo': string;
+        'veryCloseTo': string;
+        'somewhatCloseTo': string;
+        'notSoCloseTo': string;
+        'farFrom': string;
+        'veryFarFrom': string;
+        'extremelyFarFrom': string;
       }
-    },
+    };
     'backgroundColor': {
-      'notGreen': string,
-      'lightestGreen': string,
-      'veryLightGreen': string,
-      'lightGreen': string,
-      'darkestGreen': string
-    },
-    'down': string,
-    'up': string
+      'notGreen': string;
+      'lightestGreen': string;
+      'veryLightGreen': string;
+      'lightGreen': string;
+      'darkestGreen': string;
+    };
+    'down': string;
+    'up': string;
   }
 };
 
