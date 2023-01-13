03f9b98434338b59e282df2e588df8351f0ca1cf
phet-dev <phetadmin@gmail.com>
2023-01-13 04:29:55 -0700

    update copyright dates from daily grunt work

--- js/discover/view/ChallengeRatioComboBoxNode.ts
+++ js/discover/view/ChallengeRatioComboBoxNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2023, University of Colorado Boulder
 
 /**
  * This composes a ComboBox to provide a separate PDOM Node as a heading
