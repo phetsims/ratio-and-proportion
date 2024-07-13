7a2d7b3f85ea9bc3609dfe2c91923395529c48cd
phet-dev <phetadmin@gmail.com>
2024-07-13 04:31:58 -0600

    update copyright dates from daily grunt work

--- js/common/view/sound/ViewSounds.ts
+++ js/common/view/sound/ViewSounds.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2024, University of Colorado Boulder
 
 /**
  * A collection of the sounds associated with sim-specific view components making sounds. In general these have nothing
