2bcff6ef56f854fdf8cd2f398dc632379c4c5175
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-27 15:21:53 -0700

    move browser files in chipper into a sub directory, https://github.com/phetsims/chipper/issues/1534

--- js/ratio-and-proportion-tests.ts
+++ js/ratio-and-proportion-tests.ts
@@ -6,7 +6,7 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
+import qunitStart from '../../chipper/js/browser/sim-tests/qunitStart.js';
 import './common/view/describers/TickMarkDescriberTests.js';
 import './common/view/getKeyboardInputSnappingMapperTests.js';
 
