fe22c7f9d6e470cb37ee580da77c74bf91013636
phet-dev <phetadmin@gmail.com>
2025-04-03 03:26:28 -0600

    update copyright dates from daily grunt work

--- js/common/model/RAPRatioTuple.ts
+++ js/common/model/RAPRatioTuple.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2024, University of Colorado Boulder
+// Copyright 2020-2025, University of Colorado Boulder
 
 /**
  * Data type that holds both terms of the ratio, with convenience functions
